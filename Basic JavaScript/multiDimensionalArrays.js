eed6de2a4fe005c9874b308f0f939f134dcedd71
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2022-12-19 22:54:23 -0300

    Committing Modified Files!

---
+++ Basic JavaScript/multiDimensionalArrays.js
@@ -0,0 +1,17 @@
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+  ];
+
+  console.log(myArray[3][1]); // 8
+
+ /*This is what it looks like in tabular form.
+
+ Position	  0	1	2	3  
+ 0	        1	4	7	10 
+ 1	        2	5	8	11 
+ 2	        3	6	9	12 
+ 
+ */
