6adb0a43dcda0a85b270a9ea5a148b3ee03a9f04
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2022-12-25 21:50:59 -0300

    Adding some JS filesgit add .!

---
+++ Basic JavaScript/returnFunction.js
@@ -0,0 +1,14 @@
+/*
+
+Return a Value from a Function with Return
+We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+*/
+
+function timesFive (a){
+    return a * 5;
+  }
+  
+  const ansewr = timesFive(89);
+
+  console.log(ansewr);
