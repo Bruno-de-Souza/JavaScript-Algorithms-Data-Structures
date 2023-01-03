3f4e31b54c81d6e566d9f235413ed339917abf1a
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-01-02 23:49:32 -0300

    Committing JS exercises!

---
+++ Basic JavaScript/dogObject.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Budy",
+    legs: 4,
+    tails: 1,
+    friends: ["Eva", "Paloma", "Bruno"]
+  
+    // Only change code above this line
+  };
---
+++ Basic JavaScript/testObj.js
@@ -0,0 +1,8 @@
+const testObjs = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+testObjs["an entree"] = "veggie burger";
+testObjs["the drink"] = "water";
