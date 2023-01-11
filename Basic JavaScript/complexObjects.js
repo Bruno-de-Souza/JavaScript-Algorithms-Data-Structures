40c1bc16ccd57e829d9a1c8c13587e7f972bb8e5
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-01-10 23:38:28 -0300

    Committing JS exercises!

---
+++ Basic JavaScript/complexObjects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Bruno Moreira",
+      "title": "Hello World",
+      "release_year": 2023,
+      "formats": [
+        "JS",
+        "Salesforce",
+        "Commitiment"
+      ]
+    }
+  ];
+
+  console.log(myMusic);
