bc8bf7d3e1f036bd03f4c603ac13359363e332bf
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-01-13 23:46:24 -0300

    Commitign JS exercises changes

---
+++ Basic JavaScript/accessingNestedArrays.js
@@ -0,0 +1,22 @@
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[0].type;
+
+  console.log(secondTree);
