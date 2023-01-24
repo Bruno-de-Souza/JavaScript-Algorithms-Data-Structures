601954d152ec4a0e16b11e23f25b1ac8be2a8efa
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-01-23 21:36:41 -0300

    Committting JS exercises.

--- usingRecursion.js
+++ usingRecursion.js
@@ -1,3 +1,18 @@
+// Description: Using loop
+
+function sumUsingLoop(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(sumUsingLoop([2, 3, 4], 3));
+console.log(sumUsingLoop([2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// Description: Using Recursion
+
 function sum(arr, n) {
   if (n <= 0) {
       return 0;
@@ -6,5 +21,5 @@ function sum(arr, n) {
     }
   }
 
-  console.log(sum([2, 3, 4], 3));
-  console.log(sum([2, 3, 4, 5], 3));
+console.log(sum([2, 3, 4], 3));
+console.log(sum([2, 3, 4, 5, 6, 7, 8, 9, 10], 9));
