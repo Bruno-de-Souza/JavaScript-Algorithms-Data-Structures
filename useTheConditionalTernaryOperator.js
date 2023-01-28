64306c0f65f52c0de12b255830d302956d6bfb32
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-01-27 22:21:07 -0300

    Committing JS files.

---
+++ useTheConditionalTernaryOperator.js
@@ -0,0 +1,18 @@
+/*
+
+Use the Conditional (Ternary) Operator
+The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+
+The syntax is a ? b : c, where a is the condition, b is the code to run when the condition returns true, and c is the code to run when the condition returns false.
+
+The following function uses an if/else statement to check a condition:
+
+*/
+
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+}
+  
+checkEqual(1, 2);
+
+console.log(checkEqual(1, 2));
