c35c5cff2fd3a656678dd86240b2cca570bf1656
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2023-02-11 22:27:39 -0300

    Committing JS Exercises

---
+++ mutateAnArrayDeclaredWithConst.js
@@ -0,0 +1,13 @@
+const s = [5, 7, 2]; // s is an array of numbers
+function editInPlace() { // editInPlace is a function
+  // Only change code below this line
+  s[0] = 2; 
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace(s); // call the function
+
+console.log(s); // print the array
